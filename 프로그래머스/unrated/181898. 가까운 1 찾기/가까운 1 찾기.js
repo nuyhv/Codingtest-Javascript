590c884dac44fd638f2c228faee2452bb604919a
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-23 21:24:03 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,13 +1,3 @@
 function solution(arr, idx) {
-    var answer = 0;
-    
-    for (let i = idx; i < arr.length; i++) {
-        if (arr[i] === 1) {
-            answer = i;
-            break;
-        } else {
-            answer = -1;
-        }
-    }
-    return answer;
+    return arr.indexOf(1, idx)
 }
