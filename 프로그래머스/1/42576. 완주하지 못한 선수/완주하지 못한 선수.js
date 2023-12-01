14810fb42345c80842d8410da07b6090808ec9dd
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-12-01 22:37:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.71 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,20 @@
+function solution(participant, completion) {
+    const list = new Map();
+
+    for (const name of participant) {
+        list.set(name, (list.get(name) || 0) + 1);
+    }
+
+    for (const name of completion) {
+        const count = list.get(name);
+        if (count === 1) {
+            list.delete(name);
+        } else {
+            list.set(name, count - 1);
+        }
+    }
+
+    const answer = [...list.keys()].toString();
+    
+    return answer;
+}
