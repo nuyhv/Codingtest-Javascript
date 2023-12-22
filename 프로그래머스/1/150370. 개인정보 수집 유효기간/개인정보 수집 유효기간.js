d795d7ee9436bfdd155f64b3835c6d3345995c31
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-12-22 22:23:02 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.58 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,22 @@
+function solution(today, terms, privacies) {
+    const term = {};
+    const expired = [];
+    
+    terms.forEach(data => {
+        const [type, month] = data.split(' ');
+        term[type] = month * 28;
+    });
+    
+    const convertToDay = (y, m, d) => +y * 12 * 28 + +m * 28 + +d;
+    
+    for (let i = 0; i < privacies.length; i++) {
+        const [date, t] = privacies[i].split(' ');
+        const [y, m, d] = date.split('.');
+        const [ty, tm, td] = today.split('.');
+        
+        if (convertToDay(+y, +m, +d) + term[t] <= convertToDay(+ty, +tm, +td)) {
+            expired.push(i + 1);
+        }
+    }
+    return expired;
+}
