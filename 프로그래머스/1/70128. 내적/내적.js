a208955d01379c911a5712503c08123da2af2a46
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-12-25 23:06:25 +0900

    [level 1] Title: 내적, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    const len = a.length;
+    let ans = 0;
+    
+    let i = 0;
+    while (i < len) {
+        ans += a[i]*b[i];
+        i++
+    }
+    
+    return ans;
+}
