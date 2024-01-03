7d600f42bfbdc8b93060af8555b4be71a5dc0434
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-01-03 21:30:30 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,36 @@
+function solution(board, moves) {
+    const transpose = (matrix) => {
+        const rows = matrix.length;
+        const cols = matrix[0].length;
+
+        const result = Array.from({ length: cols }, () => Array(rows).fill(0));
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result.map(row => row.filter(element => element !== 0));
+    }
+
+    let newBoard = transpose(board);
+
+    const len = board.length;
+    let bucket = [];
+    let count = 0;
+
+    for (let i = 0; i < moves.length; i++) {
+        const bNum = moves[i] - 1;
+        if (newBoard[bNum].length > 0) {
+            const n = newBoard[bNum].shift();
+            if (bucket.length > 0 && bucket[bucket.length - 1] === n) {
+                bucket.pop();
+                count += 2;
+            } else {
+                bucket.push(n);
+            }
+        }
+    }
+
+    return count;
+}
