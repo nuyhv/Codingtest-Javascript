24066b09b440a48198e34d5cbc0f1e669af50be0
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-01-09 16:27:24 +0900

    [level 1] Title: 문자열 나누기, Time: 27.52 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    let stack = [];
+    let count = 0;
+    
+    for(let i = 0; i < s.length; i += 1){
+        stack.push(s[i]);
+        
+        const same = stack.filter((item) => item === stack[0]);
+        const diff = stack.filter((item) => item !== stack[0]);
+        
+        if (same.length === diff.length){
+            count ++;
+            stack = [];
+        }
+    }
+    
+    if (stack.length !== 0) count ++;
+    
+    return count;
+}
