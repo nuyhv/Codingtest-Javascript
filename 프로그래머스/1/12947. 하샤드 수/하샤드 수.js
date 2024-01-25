a30b1937a88a4d14668af8b35a39e4d604f6b172
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-01-25 20:02:34 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,11 @@
+function solution(x) {
+    let sum = 0;
+    let n = x;
+    
+    while (n > 0) {
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    }
+    
+    return x % sum === 0 ? true : false;
+}
