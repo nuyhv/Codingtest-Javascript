e6d9f33dec1f3cf0389e893b9579b1aae8ed8fe0
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-01-29 20:58:20 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.19 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,20 @@
+function solution(n, arr1, arr2) {
+    var answer = [];
+    
+    arr1 = arr1.map(num => '0'.repeat(n - num.toString(2).length) + num.toString(2));
+    arr2 = arr2.map(num => '0'.repeat(n - num.toString(2).length) + num.toString(2));
+    
+    answer = arr1.map((n, i) => {
+        let str = '';
+        for (let j = 0; j < n.length; j++) {
+            if (n[j] === '0' && arr2[i][j] === '0') {
+                str += ' ';
+            } else {
+                str += '#';
+            }
+        }
+        return str;
+    });
+    
+    return answer;
+}
