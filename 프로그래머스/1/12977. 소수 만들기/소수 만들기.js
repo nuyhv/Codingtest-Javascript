3f3277cd3bc519c7717f498002f9d7b05803270f
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-01-30 20:13:50 +0900

    [level 1] Title: 소수 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,29 @@
+function solution(nums) {
+    let count = 0;
+    let arr = [];
+    
+    const isPrime = (n) => {
+        if (n === 1) return false;
+        
+        for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
+            if (n % i === 0) return false;
+        }
+        
+        return true;
+    }
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                arr.push(sum);
+            }
+        }
+    }
+    
+    for (const num of arr) {
+        if (isPrime(num)) count++;
+    }
+    
+    return count;
+}
