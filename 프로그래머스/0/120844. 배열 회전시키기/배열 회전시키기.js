147eee1899caae3aa624579fdcc804bf7a8af034
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-02-07 16:26:07 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,5 @@
+function solution(numbers, direction) {
+    if (direction === 'right') return [numbers[numbers.length - 1], 
+                                       ...numbers.slice(0, numbers.length - 1)];
+    if (direction === 'left') return [...numbers.slice(1), numbers[0]];
+}
