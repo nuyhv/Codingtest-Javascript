11ca253e8b2e6520b5f348863152084d91ef87a1
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-03-05 19:40:23 +0900

    [level 0] Title: 다항식 더하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,19 @@
+function solution(polynomial) {
+    let [x, num] = [0, 0];
+    const arr = polynomial.split(' ');
+    
+    for (let i = 0; i < arr.length; i += 2) {
+        if (arr[i].includes('x')) {
+            arr[i].length > 1 ? x += parseInt(arr[i].slice(0, -1)) : x++;
+        } 
+        else num += parseInt(arr[i]);
+    }
+    
+    if (x === 0) {
+        return `${num}`;
+    } else if (num === 0) {
+        return x === 1 ? 'x' : `${x}x`;
+    } else {
+        return x === 1 ? `x + ${num}` : `${x}x + ${num}`;
+    }
+}
