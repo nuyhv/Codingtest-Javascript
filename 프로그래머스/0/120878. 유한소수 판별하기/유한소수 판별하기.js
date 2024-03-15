ad1dfd9a1b6da99e804180075c272d77461cd2c4
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-03-15 21:32:11 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    let gcd = 1;
+    for (let i = 1; i <= Math.min(a, b); i++) {
+        if (a % i == 0 && b % i == 0) gcd = i;
+    }
+
+    b /= gcd;
+
+    while (b % 2 == 0) b /= 2;
+    while (b % 5 == 0) b /= 5;
+    
+    return b == 1 ? 1 : 2;
+}
