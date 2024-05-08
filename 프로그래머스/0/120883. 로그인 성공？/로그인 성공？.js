a47a2fd61b7200e9d39d4680a1e8941dce8e7a74
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-05-08 23:06:20 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,10 +1,5 @@
 function solution(id_pw, db) {
-    const userdb = new Map();
-    
-    for (const el of db) {
-        const [id, pw] = el;
-        userdb.set(id, pw);
-    }
+    const userdb = new Map(db);
     
     if (!userdb.get(id_pw[0])) return 'fail';
     else return userdb.get(id_pw[0]) === id_pw[1] ? 'login' : 'wrong pw';
