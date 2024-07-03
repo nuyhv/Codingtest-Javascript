aa4b9cce991fbe02b208db41f0706f0204efbc00
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-07-03 14:31:41 +0900

    [level 2] Title: 타겟 넘버, Time: 0.99 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.5 MB, 시간: 1.06 ms
+메모리: 35.4 MB, 시간: 0.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 02일 09:19:24
+2024년 07월 03일 14:31:39
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,17 @@
 function solution(numbers, target) {
     var answer = 0;
+    const len = numbers.length;
     
     const dfs = (i, cur) => {
-        if (i === numbers.length) {
-            if (cur !== target) return;
-            else {
-                answer ++;
-                return;
-            }
+        if (i === len) {
+            if (cur === target) {
+                answer++;
+                return;    
+            } else return;       
         }
         
-        dfs (i + 1, cur + numbers[i]);
-        dfs (i + 1, cur - numbers[i]);
+        dfs(i + 1, cur + numbers[i]);
+        dfs(i + 1, cur - numbers[i]);
     }
     
     dfs(0, 0);
