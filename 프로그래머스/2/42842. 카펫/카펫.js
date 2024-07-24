9150b4c73f4165672d14a78a36350727847241fe
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-07-24 17:28:20 +0900

    [level 2] Title: 카펫, Time: 0.40 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,25 @@
+function solution(brown, yellow) {
+    function getDivisors(n) {
+        const divisors = [];
+        for (let i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) {
+                divisors.push([i, n / i]);
+            }
+        }
+        return divisors;
+    }
+
+    const totalTiles = brown + yellow;
+
+    const divisors = getDivisors(yellow);
+
+    for (let [h, w] of divisors) {
+        let totalW = w + 2;
+        let totalH = h + 2;
+        
+        if (totalW * totalH === totalTiles) {
+            return [totalW, totalH];
+        }
+    }
+    return [];
+}
