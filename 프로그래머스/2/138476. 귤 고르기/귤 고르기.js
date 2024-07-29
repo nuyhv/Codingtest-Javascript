5b975112e35c66728ca81fac0a80ad1e377c43c8
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-07-29 15:39:35 +0900

    [level 2] Title: 귤 고르기, Time: 37.86 ms, Memory: 45.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,19 @@
+function solution(k, tangerine) {
+    const countMap = new Map();
+    
+    for (const size of tangerine) {
+        countMap.set(size, (countMap.get(size) || 0) + 1);
+    }
+    
+    const counts = [...countMap.values()].sort((a, b) => b - a);
+    
+    let total = 0;
+    let varieties = 0;
+    for (const count of counts) {
+        total += count;
+        varieties++;
+        if (total >= k) break;
+    }
+    
+    return varieties;
+}
