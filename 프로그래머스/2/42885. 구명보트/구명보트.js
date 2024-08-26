e08dd33379518d879b35887cccd3756c8cb7ea89
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-08-27 00:46:40 +0900

    [level 2] Title: 구명보트, Time: 29.86 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    var boat = 0;
+    let i = 0;
+    let j = people.length - 1;
+
+    people.sort((a, b) => a - b);
+
+    while (i <= j) {
+        if (people[i] + people[j] <= limit) {
+            i++;
+        }
+        j--;
+        boat++;
+    }
+    
+    return boat;
+}
