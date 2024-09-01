03add0a4d987714f78f61db1ddd1294233d24320
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-09-01 15:42:22 +0900

    [level 2] Title: 스킬트리, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -0,0 +1,14 @@
+function solution(skill, skill_trees) {
+    var answer = 0;
+    
+    const skillArr = skill.split('');
+    
+    for (const skills of skill_trees) {
+        const filteredSkills = skills.split('').filter(char => skillArr.includes(char));
+        
+        if (filteredSkills.join('') === skill.slice(0, filteredSkills.length)) {
+            answer++;
+        }
+    }    
+    return answer;
+}
