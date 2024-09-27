011b565b91d52c298eb55d65553d4603881cf7b7
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-09-27 10:18:08 +0900

    [level 2] Title: 할인 행사, Time: 704.32 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,32 @@
+function solution(want, number, discount) {
+    let wants = {};
+    for (let i = 0; i < want.length; i++) {
+        wants[want[i]] = number[i];
+    }
+
+    let numDays = 10;
+    let totalDays = discount.length;
+    let count = 0;
+
+    for (let i = 0; i <= totalDays - numDays; i++) {
+        let window = discount.slice(i, i + numDays);
+        let windowMap = {};
+
+        for (let item of window) {
+            if (windowMap[item]) windowMap[item]++;
+            else windowMap[item] = 1;
+        }
+
+        let isMatch = true;
+        for (let item in wants) {
+            if (!windowMap[item] || windowMap[item] < wants[item]) {
+                isMatch = false;
+                break;
+            }
+        }
+
+        if (isMatch) count++;
+    }
+
+    return count;
+}
