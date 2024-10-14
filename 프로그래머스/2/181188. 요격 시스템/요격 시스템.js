8acd14e59301d57a13360d23e60640f9fcb8455e
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-10-14 17:40:34 +0900

    [level 2] Title: 요격 시스템, Time: 646.85 ms, Memory: 167 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -0,0 +1,16 @@
+function solution(targets) {
+    var answer = 1;
+    targets.sort((a, b) => b[0] - a[0]);
+    let ic = targets[0][0];
+    
+    for (let i = 1; i < targets.length; i++) {
+        const [s, e] = targets[i];
+
+        if (e <= ic) {
+            ic = s;
+            answer++;
+        }
+    }
+
+    return answer;
+}
