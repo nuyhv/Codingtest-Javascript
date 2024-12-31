c0226a981f4e32229fc449eaaadf866412f92538
nuyhv <129922828+nuyhv@users.noreply.github.com>
2024-12-31 16:33:17 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,22 @@
+function solution(n, words) {
+    const usedWords = new Set();
+    let prevWord = ""; 
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+
+        if (
+            (prevWord && prevWord[prevWord.length - 1] !== word[0]) || 
+            usedWords.has(word)
+        ) {
+            const turn = Math.floor(i / n) + 1; 
+            const player = (i % n) + 1; 
+            return [player, turn];
+        }
+
+        usedWords.add(word); 
+        prevWord = word; 
+    }
+
+    return [0, 0]; 
+}
