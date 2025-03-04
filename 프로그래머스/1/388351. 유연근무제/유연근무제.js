44f0b065f2dbea805a33c68e5ecd2f78c8b61edf
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-03-04 12:37:42 +0900

    [level 1] Title: 유연근무제, Time: 1.66 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -0,0 +1,26 @@
+
+
+function solution(schedules, timelogs, startday) {
+    var answer = 0;
+    function hhmmToMinutes(hhmm) {
+        let hours = Math.floor(hhmm / 100); 
+        let minutes = hhmm % 100;          
+        return hours * 60 + minutes;
+    }
+    
+    for(let i = 0; i < schedules.length; i++) {
+        let isLate = false;
+        for (let j = 0; j < 7; j++) {
+            if ((j + startday) % 7 === 6 || (j + startday) % 7 === 0) continue;    
+
+            if (hhmmToMinutes(timelogs[i][j]) > hhmmToMinutes(schedules[i]) + 10) {
+                isLate = true; 
+                break;
+            }
+        }
+        if (!isLate) answer++;
+    }
+
+    return answer;
+}
+
