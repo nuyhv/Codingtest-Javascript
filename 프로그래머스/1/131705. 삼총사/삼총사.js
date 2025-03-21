5c1e91b470bd5b2067f75265f081f66cb3cff8d2
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-03-21 13:12:25 +0900

    [level 1] Title: 삼총사, Time: 0.17 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -5,7 +5,7 @@ function solution(number) {
     for (let i = 0; i < len; i++) {
         for (let j = i + 1; j < len; j++) {
             for (let k = j + 1; k < len; k++) {
-                if (number[i] +number[j] + number[k] === 0) answer++
+                if (number[i] +number[j] + number[k] === 0) answer++;
             }
         }
     }
