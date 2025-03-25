d02b92ac593ca1c8761ff8f542eccae5397d03b8
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-03-25 19:27:10 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    let answer = []; 
+    
+    for (const command of commands) {
+        const [i, j, k] = command;
+        answer.push(array.slice(i-1, j).sort((a, b) => a - b)[k - 1]);
+    }
+    
+    return answer;
+}
