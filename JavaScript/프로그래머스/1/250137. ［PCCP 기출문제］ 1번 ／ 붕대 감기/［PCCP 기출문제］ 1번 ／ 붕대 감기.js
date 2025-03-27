8e35da11d506e53f0e1f3852be9e38e03b36c67d
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-03-27 18:30:14 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.50 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,38 @@
+function solution(bandage, health, attacks) {
+    let curHealth = health;
+    let maxTurn = attacks[attacks.length - 1][0];
+    let turn = 0;
+    let success = 0;
+    const [castingTime, HPS, bonusHeal] = bandage;
+    
+    const attackData = attacks.reduce((acc, [turn, damage]) => {
+        acc[turn] = damage;
+        return acc;
+    }, {});
+
+    while (turn <= maxTurn) {
+        if (attackData[turn]) {
+            const damage = attackData[turn]
+            curHealth -= damage;
+            success = 0;
+
+            if (curHealth <= 0) {
+                return -1;
+            }
+        } else {
+            success++;
+
+            if (success < castingTime) {
+                curHealth = Math.min(curHealth + HPS, health);
+            } else {
+                curHealth = Math.min(curHealth + HPS + bonusHeal, health);
+
+                success = 0;
+            }
+        }
+
+        turn++;
+    }
+
+    return curHealth;
+}
