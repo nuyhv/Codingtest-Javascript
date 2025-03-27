edbc39d2fb00e0a9267623645bfabc05fe0c7f32
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-03-27 18:30:22 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,20 @@
+function solution(wallpaper) {
+    const rows = wallpaper.length;
+    const cols = wallpaper[0].length;
+
+    let lux = Infinity, luy = Infinity;
+    let rdx = -1, rdy = -1;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (wallpaper[i][j] === '#') {
+                lux = Math.min(lux, j);
+                luy = Math.min(luy, i);
+                rdx = Math.max(rdx, j + 1);
+                rdy = Math.max(rdy, i + 1);
+            }
+        }
+    }
+
+    return [luy, lux, rdy, rdx];
+}
