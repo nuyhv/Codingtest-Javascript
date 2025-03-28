f2ae1b2e5566f9670913141a9522024aae2e8929
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-03-28 18:26:33 +0900

    [level 1] Title: 최소직사각형, Time: 446.12 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,14 @@
+function solution(sizes) {
+    let mw = 0;
+    let mh = 0;
+
+    for (const [w, h] of sizes) {
+        const maxSide = Math.max(w, h);
+        const minSide = Math.min(w, h);
+
+        mw = Math.max(mw, maxSide);
+        mh = Math.max(mh, minSide);
+    }
+
+    return mw * mh;
+}
