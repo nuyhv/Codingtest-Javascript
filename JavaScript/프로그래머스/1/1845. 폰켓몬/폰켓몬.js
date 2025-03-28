66da91d948a2891775310a464768137eb2f3c6f8
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-03-28 18:26:06 +0900

    [level 1] Title: 폰켓몬, Time: 6.45 ms, Memory: 35 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,7 @@
+function solution(nums) {
+    const deduplication = new Set(nums);
+    const maxSelect = nums.length / 2;
+
+    console.log(deduplication)
+    return Math.min(deduplication.size, maxSelect);
+}
