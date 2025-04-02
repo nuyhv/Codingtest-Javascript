3a8492fd25b2225dcaefd3e32757036bd57bd5be
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-04-02 18:09:14 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = new Array(52).fill(0);
+
+    for (let i = 0; i < my_string.length; i++) {
+        const charCode = my_string.charCodeAt(i);
+
+        if (65 <= charCode && charCode <= 90) answer[charCode - 65]++;
+        if (97 <= charCode && charCode <= 122) answer[charCode - 71]++;
+    }
+
+    return answer;
+}
