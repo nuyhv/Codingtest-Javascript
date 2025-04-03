cb70c7a73664610f3c3c0356d1f9759abd1e3eb3
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-04-03 18:12:09 +0900

    [level 1] Title: 실패율, Time: 5370.12 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,25 @@
+const getFailure = (stage, stages) => {
+    const totalPlayers = stages.filter(num => num >= stage).length;
+    const failedPlayers = stages.filter(num => num === stage).length;
+    
+    if (totalPlayers === 0) {
+        return 0;
+    } else {
+        return failedPlayers / totalPlayers;
+    }
+};
+
+const solution = (N, stages) => {
+    const failureRates = [];
+    
+    for (let i = 1; i <= N; i++) {
+        const failureRate = getFailure(i, stages);
+        failureRates.push({ stage: i, failureRate });
+    }
+    
+    failureRates.sort((a, b) => b.failureRate - a.failureRate || a.stage - b.stage);
+    
+    const result = failureRates.map(item => item.stage);
+    
+    return result;
+};
