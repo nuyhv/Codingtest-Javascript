e79344263e5b9e51ad773120778fb4fe04f08b88
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-04-21 19:28:16 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,24 @@
+function solution(new_id) {
+    let lowerStr = new_id.toLowerCase();
+
+    lowerStr = lowerStr.replace(/[^a-z0-9-_.]/g, '');
+
+    lowerStr = lowerStr.replace(/\.{2,}/g, '.');
+
+    lowerStr = lowerStr.replace(/^\.|\.$/g, '');
+
+    if (lowerStr === '') {
+        lowerStr = 'a';
+    }
+
+    if (lowerStr.length >= 16) {
+        lowerStr = lowerStr.slice(0, 15);
+        lowerStr = lowerStr.replace(/\.$/, '');
+    }
+
+    while (lowerStr.length <= 2) {
+        lowerStr += lowerStr[lowerStr.length - 1];
+    }
+
+    return lowerStr;
+}
