274896282886cfa358e9df54edd835b1f54391c0
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-05-13 18:09:30 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,10 @@
+SELECT 
+    D.ID, 
+    D.EMAIL, 
+    D.FIRST_NAME, 
+    D.LAST_NAME
+FROM DEVELOPERS D
+WHERE (D.SKILL_CODE & (SELECT SUM(CODE) 
+                       FROM SKILLCODES 
+                       WHERE NAME IN ('Python', 'C#'))) > 0
+ORDER BY D.ID ASC;
