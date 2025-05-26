e23ae2a82baefa903b34b5d6b5764cd1fc50472b
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-05-26 19:25:18 +0900

    [level 2] Title: 주식가격, Time: 6.01 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,17 @@
+function solution(prices) {
+    const n = prices.length;
+    const answer = new Array(n).fill(0);
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (prices[i] <= prices[j]) {
+                answer[i]++;
+            } else {
+                answer[i]++;
+                break;
+            }
+        }
+    }
+
+    return answer
+}
