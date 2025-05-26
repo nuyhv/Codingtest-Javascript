f6a1435ca5b58a6ceb4b3e310e1a4e990b8fa05f
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-05-26 19:25:05 +0900

    [level 2] Title: 프로세스, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,16 @@
+function solution(priorities, location) {
+    let answer = 0;
+    const queue = priorities.map((priority, idx) => ({priority, idx}));
+    
+    while (queue.length > 0) {
+        const cur = queue.shift();
+        
+        if (queue.some(el => el.priority > cur.priority)) queue.push(cur);
+        else {
+            answer++;
+            if (cur.idx === location) break;
+        }
+    }
+    
+    return answer;
+}
