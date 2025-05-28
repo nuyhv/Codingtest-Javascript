0c5f377a7692c106e6a4954a28151b1e086755fc
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-05-28 18:34:54 +0900

    [level 2] Title: 타겟 넘버, Time: 17.92 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+    let answer = 0;
+    const len = numbers.length;
+    
+    const dfs = (i, cur) => {
+        if (i < len) {
+            dfs(i + 1, cur + numbers[i]);
+            dfs(i + 1, cur - numbers[i]);
+        } else {
+            if (cur === target) answer++;
+        }
+    }
+    
+    dfs(0, 0);
+    
+    return answer
+}
