cb7eb4d16f3fd16e9dc8c6140071819034ab62b6
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-05-28 18:35:36 +0900

    [level 3] Title: 여행경로, Time: 495.10 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/3/43164. 여행경로/여행경로.js
@@ -0,0 +1,24 @@
+function solution(tickets) {
+    var answer = [];
+    const len = tickets.length;
+    const visited = Array(len).fill(false);
+    
+    const dfs = (routes) => {
+        if (routes.length === len + 1) answer.push(routes);
+        
+        for (const i in tickets) {
+            const [start, end] = tickets[i];
+            if (!visited[i]) {
+                if (routes.at(-1) === start) {
+                    visited[i] = true;
+                    dfs([...routes, end]);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+    
+    dfs(['ICN']);
+    
+    return answer.sort()[0]
+}
