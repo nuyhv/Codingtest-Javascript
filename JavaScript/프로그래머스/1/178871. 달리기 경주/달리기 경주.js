48fc4c663e669942873473cfd1c72323217938b7
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-05-30 19:17:16 +0900

    [level 1] Title: 달리기 경주, Time: 197.59 ms, Memory: 84 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,23 @@
+function solution(players, callings) {
+    const playerIndex = {};
+
+    for (let i = 0; i < players.length; i++) {
+        playerIndex[players[i]] = i;
+    }
+
+    for (let i = 0; i < callings.length; i++) {
+        const calledPlayer = callings[i];
+        const calledIdx = playerIndex[calledPlayer];
+
+        if (calledIdx > 0) {
+            const temp = players[calledIdx];
+            players[calledIdx] = players[calledIdx - 1];
+            players[calledIdx - 1] = temp;
+
+            playerIndex[players[calledIdx]] = calledIdx;
+            playerIndex[players[calledIdx - 1]] = calledIdx - 1;
+        }
+    }
+
+    return players
+}
