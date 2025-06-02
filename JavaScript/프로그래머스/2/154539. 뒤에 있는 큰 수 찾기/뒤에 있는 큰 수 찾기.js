8b5716d98865ecd441fb0555a055c34fec680c90
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-02 19:47:16 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 67.78 ms, Memory: 126 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = new Array(numbers.length).fill(-1);
+    let stack = [];
+    for (let i = 0; i < numbers.length; i++){
+        while (stack && numbers[stack[stack.length - 1]] < numbers[i]){
+            answer[stack.pop()] = numbers[i];
+        }
+        stack.push(i);
+    }
+    return answer
+}
