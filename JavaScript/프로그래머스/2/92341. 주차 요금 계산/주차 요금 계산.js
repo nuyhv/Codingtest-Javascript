5efcbae3a7abb6f6ebcb6d7e3d746211adba153f
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-03 19:46:50 +0900

    [level 2] Title: 주차 요금 계산, Time: 707.55 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -0,0 +1,42 @@
+function solution(fees, records) {
+    var answer = [];
+    const [기본시간, 기본요금, 단위시간, 단위요금] = fees;
+    const parkingTime = {};
+    const inTime = {};
+
+    const getMin = (time) => {
+        const [hours, minutes] = time.split(':').map(Number);
+        return hours * 60 + minutes;
+    };
+
+    for (const [시각, 차량번호, 내역] of records.map((el) => el.split(' '))) {
+        const curTime = getMin(시각);
+        
+        if (내역 === "IN") {
+            inTime[차량번호] = curTime;
+        } else {
+            const parkedTime = curTime - inTime[차량번호];
+            parkingTime[차량번호] = (parkingTime[차량번호] || 0) + parkedTime;
+            delete inTime[차량번호];
+        }
+    }
+
+    const endOfDay = getMin("23:59");
+    for (const num in inTime) {
+        const parkedTime = endOfDay - inTime[num];
+        parkingTime[num] = (parkingTime[num] || 0) + parkedTime;
+    }
+
+    const calculateFee = (time) => {
+        if (time <= 기본시간) return 기본요금;
+        return 기본요금 + Math.ceil((time - 기본시간) / 단위시간) * 단위요금;
+    };
+
+    answer = Object.keys(parkingTime)
+                   .sort((a, b) => a.localeCompare(b))
+                   .map((num) => calculateFee(parkingTime[num]));
+
+    console.log(parkingTime, inTime);
+    
+    return answer;
+}
