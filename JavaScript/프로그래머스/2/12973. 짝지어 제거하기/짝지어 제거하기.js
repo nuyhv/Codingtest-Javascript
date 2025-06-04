eb62321390230fbd8a3800055d66829c9d66b3a0
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-04 19:25:00 +0900

    [level 2] Title: 짝지어 제거하기, Time: 70.75 ms, Memory: 58.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    const stack = [];
+    for (const char of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0 ? 1 : 0;
+}
