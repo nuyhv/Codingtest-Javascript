4935d99e01774b1a726866d6a04fac86151def3f
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-04 19:24:52 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n)
+{
+    let battery = 0;
+    
+    while (n !== 0) {
+        if (n % 2 === 1) {
+            n--;
+            battery++;
+        }
+        else n /= 2;
+    }
+    
+    return battery;
+}
