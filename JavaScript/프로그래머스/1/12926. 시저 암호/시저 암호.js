619cc1286886de5d6e08183defa376e9c6973a55
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-05 19:49:25 +0900

    [level 1] Title: 시저 암호, Time: 1.73 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,18 @@
+function solution(s, n) {
+    return s
+        .split('')
+        .map(char => {
+            if (char === ' ') {
+                return char;
+            }
+
+            if (char >= 'A' && char <= 'Z') {
+                return String.fromCharCode((char.charCodeAt(0) - 'A'.charCodeAt(0) + n) % 26 + 'A'.charCodeAt(0));
+            }
+            
+            if (char >= 'a' && char <= 'z') {
+                return String.fromCharCode((char.charCodeAt(0) - 'a'.charCodeAt(0) + n) % 26 + 'a'.charCodeAt(0));
+            }
+        })
+        .join('');
+}
