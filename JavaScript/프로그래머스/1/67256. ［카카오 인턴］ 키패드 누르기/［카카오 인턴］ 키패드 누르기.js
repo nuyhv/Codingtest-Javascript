56929756685fbbcc5b451d36351b821d1a0686bc
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-07 20:34:43 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.70 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -0,0 +1,40 @@
+function solution(numbers, hand) {
+    const keypad = {
+        1: [0, 0], 2: [0, 1], 3: [0, 2],
+        4: [1, 0], 5: [1, 1], 6: [1, 2],
+        7: [2, 0], 8: [2, 1], 9: [2, 2],
+        '*': [3, 0], 0: [3, 1], '#': [3, 2],
+    };
+
+    let leftPos = '*';
+    let rightPos = '#';
+
+    const getDistance = (pos1, pos2) => {
+        const [x1, y1] = keypad[pos1];
+        const [x2, y2] = keypad[pos2];
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    };
+
+    const getResult = (number) => {
+        if (number === 1 || number === 4 || number === 7) {
+            leftPos = number;
+            return 'L';
+        } else if (number === 3 || number === 6 || number === 9) {
+            rightPos = number;
+            return 'R';
+        } else {
+            const leftDistance = getDistance(leftPos, number);
+            const rightDistance = getDistance(rightPos, number);
+
+            if (leftDistance < rightDistance || (leftDistance === rightDistance && hand === 'left')) {
+                leftPos = number;
+                return 'L';
+            } else {
+                rightPos = number;
+                return 'R';
+            }
+        }
+    };
+
+    return numbers.map(getResult).join('')
+}
