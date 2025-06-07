9d1cd64bac984aff0c1058a170131a963c3e7010
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-07 20:32:21 +0900

    [level 1] Title: 신고 결과 받기, Time: 950.55 ms, Memory: 102 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,30 @@
+function solution(id_list, report, k) {
+    const userInfo = id_list.map(id => ({ id, reported: [], mailCount: 0, warnCount: 0 }));
+
+    const reportList = [...new Set(report)].map(str => str.split(' '));
+
+    for (let i = 0; i < reportList.length; i++) {
+        const [reporter, target] = reportList[i];
+
+        const targetUser = userInfo.find(user => user.id === reporter);
+        const warnUser = userInfo.find(user => user.id === target);
+        if (targetUser) {
+            targetUser.reported.push(target);
+            warnUser.warnCount++;
+        }
+    }
+
+    const banned = userInfo
+        .filter(user => user.warnCount >= k)
+        .map(user => user.id);
+    
+    userInfo.forEach(data => {
+        data.reported.forEach(reportedId => {
+            if (banned.includes(reportedId)) {
+                data.mailCount++
+            }
+        });
+    });
+    
+    return userInfo.map(data => data.mailCount)
+}
