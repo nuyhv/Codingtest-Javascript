42d4728806ae1adbf8dcddd4c3fc25b8685fef18
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-18 23:49:14 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.53 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = '';
+    
+    let i = 0;
+    while (i < n) {
+        if (i % 2 === 0) answer += '수';
+        else answer += '박';
+        i++;
+    }
+    
+    return answer
+}
