7ca1a0e6916343d9c0af7aff27894191642cec0f
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-06-23 20:03:06 +0900

    [level 2] Title: 전화번호 목록, Time: 153.50 ms, Memory: 80.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ JavaScript/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,5 +1,5 @@
 function solution(phone_book) {
-    phone_book.sort();
+    phone_book.sort()
 
     for (let i = 0; i < phone_book.length - 1; i++) {
         if (phone_book[i + 1].startsWith(phone_book[i])) {
