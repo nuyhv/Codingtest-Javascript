a7a57693f8519f4da9ba1d2eaa5aed210c53a387
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-08-24 20:40:14 +0900

    [level 1] Title: 추억 점수, Time: 34.48 ms, Memory: 40.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,17 @@
+function solution(name, yearning, photo) {
+    let ans = [];
+    
+    for (const people of photo) {
+        let score = 0;
+        
+        for (const person of people) {
+            const idx = name.indexOf(person)
+            
+            if (idx !== -1) score += yearning[idx]
+        }
+        ans.push(score)
+    }
+    
+    console.log(name, yearning, photo, ans)
+    return ans;
+}
