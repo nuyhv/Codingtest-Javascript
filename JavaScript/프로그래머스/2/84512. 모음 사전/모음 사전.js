cb79af3187069a5bcb9b761cedd051363ddb09a7
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-13 19:39:24 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ JavaScript/프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,8 +1,8 @@
 function solution(word) {
-    const dic = ['A', 'E', 'I', 'O', 'U'];
-    const value = [781, 156, 31, 6, 1];
+    const dic = ['A', 'E', 'I', 'O', 'U']
+    const value = [781, 156, 31, 6, 1]
 
-    let pos = 0;
+    let pos = 0
 
     for (let i = 0; i < word.length; i++) {
         const index = dic.indexOf(word[i]);
