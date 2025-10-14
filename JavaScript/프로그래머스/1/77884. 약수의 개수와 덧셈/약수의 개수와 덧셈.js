f5e4d7c64944f5e3dd72ee605678ded1af4b34ca
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-14 19:26:50 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.12 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,24 @@
+function solution(left, right) {
+    let answer = 0
+    
+    const getDivisors = (n) => {
+    const divisors = [];
+
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            divisors.push(i);
+        }
+    }
+
+    return divisors;
+    }
+    
+    for (let i = left; i <= right; i++) {
+        if (getDivisors(i).length % 2 === 0) {
+            answer += i;
+        } else {
+            answer -= i;
+        }
+    }
+    return answer;
+}
