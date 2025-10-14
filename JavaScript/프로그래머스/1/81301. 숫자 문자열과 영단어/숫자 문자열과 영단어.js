67dba6ac6e6e1c39b6175bf2371482c8d29a1173
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-14 19:26:35 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,34 @@
+function solution(s) {
+    const digitToWord = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    };
+
+    let answer = ''
+    let currentWord = '';
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (isNaN(char)) {
+            currentWord += char;
+
+            if (digitToWord[currentWord]) {
+                answer += digitToWord[currentWord];
+                currentWord = '';
+            }
+        } else {
+            answer += char;
+        }
+    }
+
+    return parseInt(answer, 10);
+}
