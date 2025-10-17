29ec90c2858438e6fd8d103b7ec78b4b6ab64f6a
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-17 19:33:46 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 4.68 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,5 @@
 function solution(s) {
-    //split하고 sort 오름차순 한다음 return
-    const nums = s.split(' ').sort((a, b) => a - b);
+    const nums = s.split(' ').sort((a, b) => a - b)
     const len = nums.length;
     
     console.log(nums)
