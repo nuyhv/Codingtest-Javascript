cc5912a4559be814f43bee1bff31cc1d50e99040
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-22 20:29:44 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ JavaScript/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,5 @@
 function solution(s) {
     const len = s.length
     
-    return len % 2 === 0 ? s[len / 2 - 1] + s[len / 2] : s[Math.floor(len / 2)];
+    return len % 2 === 0 ? s[len / 2 - 1] + s[len / 2] : s[Math.floor(len / 2)]
 }
