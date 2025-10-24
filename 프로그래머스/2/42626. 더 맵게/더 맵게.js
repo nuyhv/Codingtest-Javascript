e1a618b9f74b3ac34f6d42e56942fc4bb6dc4bc0
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-24 17:07:25 +0900

    [level 2] Title: 더 맵게, Time: 485.90 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -1,83 +1,97 @@
-class MinHeap {
-    constructor() {
-        this.heap = [];
-    }
-
-    push(value) {
-        this.heap.push(value);
-        this.heapifyUp();
-    }
-
-    pop() {
-        if (this.isEmpty()) {
-            return null;
-        }
-        if (this.heap.length === 1) {
-            return this.heap.pop();
-        }
-        const root = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown();
-        return root;
-    }
-
-    isEmpty() {
-        return this.heap.length === 0;
-    }
-
-    heapifyUp() {
-        let currentIdx = this.heap.length - 1;
-        while (currentIdx > 0) {
-            const parentIdx = Math.floor((currentIdx - 1) / 2);
-            if (this.heap[currentIdx] < this.heap[parentIdx]) {
-                [this.heap[currentIdx], this.heap[parentIdx]] = [this.heap[parentIdx], this.heap[currentIdx]];
-                currentIdx = parentIdx;
-            } else {
-                break;
-            }
-        }
-    }
-
-    heapifyDown() {
-        let currentIdx = 0;
-        while (true) {
-            const leftChildIdx = 2 * currentIdx + 1;
-            const rightChildIdx = 2 * currentIdx + 2;
-            let smallestChildIdx = currentIdx;
-
-            if (leftChildIdx < this.heap.length && this.heap[leftChildIdx] < this.heap[smallestChildIdx]) {
-                smallestChildIdx = leftChildIdx;
-            }
-
-            if (rightChildIdx < this.heap.length && this.heap[rightChildIdx] < this.heap[smallestChildIdx]) {
-                smallestChildIdx = rightChildIdx;
-            }
-
-            if (smallestChildIdx !== currentIdx) {
-                [this.heap[currentIdx], this.heap[smallestChildIdx]] = [this.heap[smallestChildIdx], this.heap[currentIdx]];
-                currentIdx = smallestChildIdx;
-            } else {
-                break;
-            }
-        }
-    }
-}
-
 function solution(scoville, K) {
-    let count = 0;
-    const heap = new MinHeap();
+    class MinHeap {
+        constructor() {
+            this.heap = []
+        }
 
-    for (const value of scoville) {
-        heap.push(value);
+        push(value) {
+            this.heap.push(value);
+            this.heapifyUp();
+        }
+
+        pop() {
+            if (this.isEmpty()) {
+                return null;
+            }
+
+            const root = this.heap[0];
+            const last = this.heap.pop();
+
+            if (this.heap.length > 0) {
+                this.heap[0] = last;
+                this.heapifyDown();
+            }
+
+            return root;
+        }
+
+        isEmpty() {
+            return this.heap.length === 0;
+        }
+
+        heapifyUp() {
+            let currentIndex = this.heap.length - 1;
+
+            while (currentIndex > 0) {
+                const parentIndex = Math.floor((currentIndex - 1) / 2);
+
+                if (this.heap[currentIndex] < this.heap[parentIndex]) {
+                    this.swap(currentIndex, parentIndex);
+                    currentIndex = parentIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        heapifyDown() {
+            let currentIndex = 0;
+
+            while (true) {
+                const leftChildIndex = 2 * currentIndex + 1;
+                const rightChildIndex = 2 * currentIndex + 2;
+                let smallestChildIndex = currentIndex;
+
+                if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallestChildIndex]) {
+                    smallestChildIndex = leftChildIndex;
+                }
+
+                if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallestChildIndex]) {
+                    smallestChildIndex = rightChildIndex;
+                }
+
+                if (smallestChildIndex !== currentIndex) {
+                    this.swap(currentIndex, smallestChildIndex);
+                    currentIndex = smallestChildIndex;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        swap(i, j) {
+            [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+        }
     }
+
+    const heap = new MinHeap();
+    scoville.forEach(value => heap.push(value));
+
+    let mixCount = 0;
 
     while (heap.heap.length > 1 && heap.heap[0] < K) {
         const first = heap.pop();
         const second = heap.pop();
-        const mixed = first + (second * 2);
-        heap.push(mixed);
-        count++;
+
+        const newScoville = first + (second * 2);
+        heap.push(newScoville);
+
+        mixCount++;
     }
 
-    return heap.heap[0] >= K ? count : -1;
+    if (heap.heap[0] < K) {
+        return -1;
+    }
+
+    return mixCount
 }
