2506e9ffc4de0aa3a1cc4bb6f191b768c0392f31
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-27 19:42:13 +0900

    [level 2] Title: 카펫, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42842. 카펫/카펫.js
+++ JavaScript/프로그래머스/2/42842. 카펫/카펫.js
@@ -1,6 +1,6 @@
 function solution(brown, yellow) {
     function getDivisors(n) {
-        const divisors = [];
+        const divisors = []
         for (let i = 1; i <= Math.sqrt(n); i++) {
             if (n % i === 0) {
                 divisors.push([i, n / i]);
