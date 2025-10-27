714f4b9ef8acbc792d4935d32d3ad3776757d3a5
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-10-27 19:23:14 +0900

    [level 2] Title: 올바른 괄호, Time: 31.42 ms, Memory: 38.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ JavaScript/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -2,7 +2,7 @@ function solution(s) {
     const stack = [];
     
     [...s].map(str => {
-        (str === ')' && stack[stack.length - 1] === '(') ? stack.pop() : stack.push(str);
+        (str === ')' && stack[stack.length - 1] === '(') ? stack.pop() : stack.push(str)
     })
     
     return stack.length === 0
