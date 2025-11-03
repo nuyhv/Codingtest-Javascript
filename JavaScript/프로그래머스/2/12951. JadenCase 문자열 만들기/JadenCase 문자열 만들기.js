ed7b60d856f92b0bdf19f468b210de2f2f785dec
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-03 18:45:40 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ JavaScript/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -5,5 +5,5 @@ function solution(s) {
             if (word.length === 0) return ''
             return word[0].toUpperCase() + word.slice(1).toLowerCase();
         })
-        .join(' '); 
+        .join(' ')
 }
