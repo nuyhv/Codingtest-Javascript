b31b47d382835b13d6bb220a3806e51fac286c7d
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-08 19:08:02 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,10 @@
+function solution(n, m) {
+    const getGcd = (a, b) => {
+        if (b === 0) return a;
+        return getGcd(b, a % b);
+    }
+    const gcd = getGcd(n, m);
+    const lcm = Math.abs(n * m) / gcd;
+    
+    return [gcd, lcm]
+}
