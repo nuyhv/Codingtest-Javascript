eaa3616218884f78d2d90ff79f282cbbe0927a92
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-08 19:08:46 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ JavaScript/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,5 +1,5 @@
 function solution(n) {
     const sqrt = Math.sqrt(n)
     
-    return sqrt === Math.ceil(sqrt) ? Math.pow(sqrt + 1, 2) : -1;
+    return sqrt === Math.ceil(sqrt) ? Math.pow(sqrt + 1, 2) : -1
 }
