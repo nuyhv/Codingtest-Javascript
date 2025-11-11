df78e0cb00f2a3c45fd93b28b693994c65881b7c
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-11 19:49:11 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.18 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    const answer = []
+    const lastIdx = new Map(); // [char, index]
+
+    for (let i = 0; i < s.length; i++) {
+        const curChar = s[i];
+
+        if (lastIdx.has(curChar)) {
+            answer.push(i - lastIdx.get(curChar));
+        } else {
+            answer.push(-1);
+        }
+
+        lastIdx.set(curChar, i);
+    }
+
+    return answer;
+}
