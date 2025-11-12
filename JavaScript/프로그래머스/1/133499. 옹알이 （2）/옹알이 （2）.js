5fe0bbd412c24c962bb73507cfb27f2e107cb2f2
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-12 19:48:22 +0900

    [level 1] Title: 옹알이 (2), Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,11 @@
+function solution(babbling) {
+    let answer = 0
+
+    for (let word of babbling) {
+        if (/(aya|ye|woo|ma)\1+/g.test(word)) continue;
+        if (/^(aya|ye|woo|ma)+$/g.test(word)) {
+            answer++;
+        }
+    }
+    return answer;
+}
