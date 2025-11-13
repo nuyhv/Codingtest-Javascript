00f2d662ed026c0e9cf3adddf52e4ac61c0651d8
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-13 20:03:34 +0900

    [level 2] Title: 조이스틱, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ JavaScript/프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -2,7 +2,7 @@ function solution(name) {
     function getCharMoveCount(char) {
         const fromA = char.charCodeAt(0) - 'A'.charCodeAt(0)
         const fromZ = 'Z'.charCodeAt(0) - char.charCodeAt(0) + 1
-        return Math.min(fromA, fromZ);
+        return Math.min(fromA, fromZ)
     }
 
     let answer = 0
