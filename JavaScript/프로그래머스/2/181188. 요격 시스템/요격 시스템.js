f5106a9534449380c82bbb1db2ae20ebee7fc1b6
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-14 21:09:27 +0900

    [level 2] Title: 요격 시스템, Time: 494.20 ms, Memory: 168 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ JavaScript/프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,6 +1,6 @@
 function solution(targets) {
     var answer = 1
-    targets.sort((a, b) => b[0] - a[0]);
+    targets.sort((a, b) => b[0] - a[0])
     let ic = targets[0][0];
     
     for (let i = 1; i < targets.length; i++) {
