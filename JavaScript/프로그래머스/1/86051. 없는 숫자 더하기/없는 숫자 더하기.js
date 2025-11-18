2f27f5a755901562766db90d036eaf8d7bae0927
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-18 19:39:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ JavaScript/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -2,5 +2,5 @@ function solution(numbers) {
     const arr = new Array(10).fill(0).map((num, i) => num + i)
     const diff = arr.filter(num => !numbers.includes(num))
     
-    return diff.reduce((acc, cur) => acc + cur);
+    return diff.reduce((acc, cur) => acc + cur)
 }
