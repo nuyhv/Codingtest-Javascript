f3e551191ff9ec60721c4eecda52fd1f5ee93a67
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-19 19:41:33 +0900

    [level 1] Title: 음양 더하기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,5 +1,5 @@
 function solution(absolutes, signs) {
     const arr = absolutes.map((num, i) => signs[i] ? num : -num)
     
-    return arr.reduce((acc, cur) => acc + cur, 0);
+    return arr.reduce((acc, cur) => acc + cur, 0)
 }
