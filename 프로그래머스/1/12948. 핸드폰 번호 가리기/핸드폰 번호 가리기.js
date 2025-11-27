0a2bb1c5dd17933c2fd7e82de09de342b76687e7
nuyhv <129922828+nuyhv@users.noreply.github.com>
2025-11-27 23:12:06 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,3 @@
 function solution(phone_number) {
-    const len = phone_number.length;
-    const lastNum = phone_number.slice(len - 4);
-    
-    return '*'.repeat(len - 4) + lastNum;
+    return phone_number.replace(/\d(?=\d{4})/g, "*")
 }
